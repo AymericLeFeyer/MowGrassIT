48dd58203cadc4080a15ceb76a7b58f40eeb8bdc
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-05-14 02:33:01 +0200

    local deployment

---
+++ build/precache-manifest.6cb8bf3ffbc51c197a5eb23bd3c2a54d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed4dbc6f6fce7cad512f08374d65c4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f56f61bbe15b67ee5eb7",
+    "url": "./static/css/main.05c758b6.chunk.css"
+  },
+  {
+    "revision": "6e9d9b7d1a60edbcd078",
+    "url": "./static/js/2.aa7ff2f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.aa7ff2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56f61bbe15b67ee5eb7",
+    "url": "./static/js/main.71337097.chunk.js"
+  },
+  {
+    "revision": "931088e38987d6f4b0e1",
+    "url": "./static/js/runtime-main.72e4cf6b.js"
+  },
+  {
+    "revision": "10eebb46b0a36470e0305773e5d70bbb",
+    "url": "./static/media/subject.10eebb46.pdf"
+  }
+]);
